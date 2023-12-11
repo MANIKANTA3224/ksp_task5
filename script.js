b22e9e9c6ff7f68d0b33e26e8c4d52352babae95
MANIKANTA3224 <153202534+MANIKANTA3224@users.noreply.github.com>
2023-12-11 22:24:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+console.log("hello world");
+let n=10;
+console.log(n);//10
+let n1=20;//integer
+let s="TARAK";//string
+let bool=true;//boolean
+console.log(n1);
+console.log(s);
+console.log(bool);
+const x=10;
+console.log(x);
+//arthimetic operators
+let y=20;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x**y);
+console.log(x/y);
+//increment and decrement
+let n2=20;
+n2++;
+console.log(n2);
+let n3=10;
+++n3;
+console.log(n3);
+let n4=11;
+--n4;
+console.log(n4);
+let n5=12;
+n5--;
+console.log(n5);
+//assignment operators
+let a=2;
+a+=20;
+console.log(a);
+let b=20;
+b-=10;
+console.log(b);
+//comparison operator
+let c=10;
+let d=20;
+console.log(c==d);
+console.log(c===d);
+console.log(c>d);
+console.log(c<d);
+console.log(c>=d);
+console.log(c<=d);
+console.log(c!=d);
+
+
